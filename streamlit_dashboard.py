a550ad391c38e3b5f279779d5dcc9ab9c4149ba0
Kevin Jaramillo <106689567+KevinJara2001@users.noreply.github.com>
2025-07-27 23:52:24 -0500

    Update streamlit_dashboard.py

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -9,7 +9,7 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 st.set_page_config(page_title="Dashboard Notaría", layout="wide")
-st.title("Dashboard para Predicción de Servicios Notariales")
+st.title("Dashboard para la Predicción de Servicios Notariales")
 st.markdown("---")
 
 @st.cache_data
