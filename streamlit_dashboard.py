55f6ff9f7c4a0a8201e46f1a8f3c2050f4888066
Kevin Jaramillo <106689567+KevinJara2001@users.noreply.github.com>
2025-07-28 00:16:16 -0500

    Update streamlit_dashboard.py

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -10,7 +10,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import gc
 
 st.set_page_config(page_title="Dashboard Notaría", layout="wide")
-st.title("Dashboard Predicción de Servicios Notariales")
+st.title("Dashboard Predicción Servicios Notariales")
 st.markdown("---")
 
 @st.cache_data
