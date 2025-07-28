5fb81a14e838a0d200e61dd66d605d37f8a10929
Kevin Jaramillo <106689567+KevinJara2001@users.noreply.github.com>
2025-07-28 02:13:22 -0500

    Delete Home.py

--- Home.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Dashboard Notaría", layout="wide")
-st.title("📊 Dashboard de Predicción de Servicios Notariales")
-st.markdown("---")
-st.write("Selecciona una página en el menú lateral para explorar datos, modelos y predicciones.")
