fbd0fb790258fc4acaf9333a22f31ff31993abbc
Kevin Jaramillo <106689567+KevinJara2001@users.noreply.github.com>
2025-07-28 01:24:30 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title('🏠 Bienvenido al Dashboard de la Notaría')
+st.title('Bienvenido al Dashboard de la Notaría')
 st.markdown("""
 Este panel presenta visualizaciones interactivas y predicciones sobre la demanda de servicios notariales usando Machine Learning.
 
